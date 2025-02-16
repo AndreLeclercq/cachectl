93af5f970c872d42168b8c7a9677f606b923ad22
Andre LECLERCQ <andre.leclercq@oclock.io>
2025-02-16 22:08:51 +0100

    create fs mod

---
+++ src/fs.rs
@@ -0,0 +1,26 @@
+use std::io::Result;
+use std::path::{Path, PathBuf};
+
+// List cache files.
+pub fn list_cache_files() -> Result<Vec<PathBuf>> {
+
+    Ok(())
+}
+
+// Get file size.
+pub fn get_file_size(path: &Path) -> Result<u64> {
+
+    Ok(())
+}
+
+// Go to directory.
+pub fn go_to(_path: &Path) -> Result<()> {
+
+    Ok(())
+}
+
+// Remove file.
+pub fn remove_file(_path: &Path) -> Result<()> {
+
+    Ok(())
+}
